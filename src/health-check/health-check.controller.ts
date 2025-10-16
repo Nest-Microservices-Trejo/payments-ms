d2f7b0c04cb29a7a5fc27d97246a5745f3453cfc
josers77j <josers77.1@gmail.com>
2025-10-16 16:55:22 -0600

    CI CD test

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthCheckController {
   @Get()
   healthCheck() {
-    return 'client gateway is running';
+    return 'payments ms is running';
   }
 }
